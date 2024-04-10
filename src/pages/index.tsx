b8a32adcebdda7a2a0692c1a168da68c9238777f
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-10 20:32:45 +0200

    new Text and Contact me and home site finisched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect";
 import { Button } from "@/components/ui/button";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export default function Home() {
+  // TYPEWRITER EFFECT
   const words = [
     {
       text: "Programmer",
@@ -13,12 +13,12 @@ export default function Home() {
     },
   ];
 
-  const [, setShowTypewriter] = useState(false);
+  // CONTACT ME BUTTON FUNKTION 
+  const handleContactMeClick = () => {
+    window.location.href = "mailto:leoradtke@outlook.de";
+  };
 
-  useEffect(() => {
-    // Show typewriter effect after component mounts
-    setShowTypewriter(true);
-  }, []);
+
 
   return (
     <>
@@ -26,16 +26,13 @@ export default function Home() {
 
       <div className="custom-margin text-5xl font-bold text-white">
         <h1>
-          Hi,I'm Leo,{" "}
+          Hi, I'm Leo,{" "}
           <span className="text-indigo-600 ">
             {" "}
             <TypewriterEffectSmooth words={words} />{" "}
           </span>{" "}
         </h1>
       </div>
-
-
-
 
       <div className="custom-margin-little-text text-white ">
         <p className="text-white-700 text-sm sm:text-base font-bold ">
@@ -45,26 +42,22 @@ export default function Home() {
 
       <div className="text-white custom-new-text">
         <p>
-          My name is Leo and I am 14 years old.<br />I have been programming
-          since I was 10 <br></br>and have been influenced in many areas of computer
-          science.<br />I started with game programming, I continued with
-          Python and many <br />programming languages.<br />After that I started
-          with the backend and frontend of web development. <br></br> React, Typescript,
-          JavaScript, HTML, CSS are common things I work with.
+          Leo is 14 years old and has been programming since he was 10. <br />
+          He is proficient in several programming languages, including Python, <br />
+          and has experience in web development (backend and frontend). <br />
+          Specifically, he works with React, TypeScript, JavaScript, HTML and CSS and Python.
         </p>
       </div>
 
       <div className="custom-button">
-        <Button variant="outline">Contact Me</Button>
+        <Button variant="outline" onClick={handleContactMeClick}>Contact Me</Button>
       </div>
 
-      
       <div className="custom-margin-image-2">
         <img src="\programmer-image.jpg" className="small-logo" />
       </div>
 
-
-
     </>
   );
+
 }

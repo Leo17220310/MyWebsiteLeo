cd1a7b559c3c6e7b6138817fd0aa3c58e20f195f
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-12 22:28:38 +0200

    new Web-Development React and bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect";
 import { Button } from "@/components/ui/button";
-
-
+import React from "react";
+import { StickyScroll } from "../components/ui/scroll-reveal";
+import Image from "next/image";
 
 export default function Home() {
   // TYPEWRITER EFFECT
+  const webDevelopmentRef = useRef(null);
+
   const words = [
     {
       text: "Programmer",
@@ -13,12 +16,43 @@ export default function Home() {
     },
   ];
 
-  // CONTACT ME BUTTON FUNKTION 
+  // CONTACT ME BUTTON FUNCTION
   const handleContactMeClick = () => {
     window.location.href = "mailto:leoradtke@outlook.de";
   };
 
-
+  const content = [
+    {
+      title: "Get Summary",
+      description:
+        "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      content: (
+        <div className="h-full w-full">
+          <img
+            src="\screenWebsiteSummary.png"
+            className="w-96 h-100"
+            alt="Your first image alt text"
+          />
+        </div>
+      ),
+    },
+    {
+      title: "Altes Portfolio",
+      description:
+        "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      content: (
+        <div className="h-full w-full">
+          <img
+            src="\programmer-image.jpg"
+            className="h-full w-full object-cover"
+            alt="Your second image alt text"
+          />
+        </div>
+      ),
+    },
+  ];
+  
+  
 
   return (
     <>
@@ -57,7 +91,20 @@ export default function Home() {
         <img src="\programmer-image.jpg" className="small-logo" />
       </div>
 
+      <div
+        ref={webDevelopmentRef}
+        className="custom-margin text-4xl font-bold text-white web-development-heading"
+      >
+        <h2>Web-Development</h2>
+      </div>
+
+      <div className="p-10">
+        <StickyScroll content={content} />
+      </div>
+
+      <div className="python-Heading text-white font-bold text-4xl">
+        <h3>Python-Projects</h3>
+      </div>
     </>
   );
-
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
-import type { Config } from "tailwindcss"
+// tailwind.config.js
 
-const config = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -72,12 +74,9 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-
-      
     },
-    
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+};
 
-export default config
+export default config;

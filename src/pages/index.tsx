8dd076f31fd34b014d37aa3a0fa876094d22b781
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-16 20:17:39 +0200

    new only tailwind tile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const words = [
     {
-      text: "Programmer", 
+      text: "Programmer",
       className: "text-indigo-600",
     },
   ];
@@ -38,7 +38,8 @@ export default function Home() {
     },
     {
       title: "Old Portfolio",
-      description: "Welcome to my old portfolio, a labor of love hosted on GitHub. Crafted as a fun project to refine my HTML and CSS skills, it showcases my journey in web development. Explore its pages to witness the evolution of my abilities and the projects I've undertaken. Though it's a relic of the past, it remains a testament to my dedication to mastering the craft. Here's the Link: https://leo17220310.github.io/Portfolio/ .",
+      description:
+        "Welcome to my old portfolio, a labor of love hosted on GitHub. Crafted as a fun project to refine my HTML and CSS skills, it showcases my journey in web development. Explore its pages to witness the evolution of my abilities and the projects I've undertaken. Though it's a relic of the past, it remains a testament to my dedication to mastering the craft. Here's the Link: https://leo17220310.github.io/Portfolio/ .",
       content: (
         <div className="h-full w-full">
           <img
@@ -47,21 +48,18 @@ export default function Home() {
             alt="Your second image alt text"
           />
         </div>
-      )
-    }
-    
-    
-    
-    
+      ),
+    },
   ];
-  
-  
 
   return (
     <>
+
+    
+    
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <div className="custom-margin text-5xl font-bold text-white">
+      <div className="mt-[300px] ml-[210px] text-5xl font-bold text-white">
         <h1>
           Hi, I'm Leo !{" "}
           <span className="text-indigo-600 ">
@@ -71,26 +69,30 @@ export default function Home() {
         </h1>
       </div>
 
-      <div className="custom-margin-little-text text-white ">
+      <div className="mt-[-200px] ml-[210px] text-white font-bold">
         <p className="text-white-700 text-sm sm:text-base font-bold ">
           Create your Program like a pro.
         </p>
       </div>
 
-      <div className="text-white custom-new-text">
+      <div className="text-white  mt-[200px] ml-[210px]">
         <p>
           Leo is 14 years old and has been programming since he was 10. <br />
-          He is proficient in several programming languages, including Python, <br />
+          He is proficient in several programming languages, including Python,{" "}
+          <br />
           and has experience in web development (backend and frontend). <br />
-          Specifically, he works with React, TypeScript, JavaScript, HTML and CSS and Python.
+          Specifically, he works with React, TypeScript, JavaScript, HTML and
+          CSS and Python.
         </p>
       </div>
 
-      <div className="custom-button">
-        <Button variant="outline" onClick={handleContactMeClick}>Contact Me</Button>
+      <div className="mt-[30px] ml-[210px]">
+        <Button variant="outline" onClick={handleContactMeClick}>
+          Contact Me
+        </Button>
       </div>
 
-      <div className="custom-margin-image-2">
+      <div className="w-[400px] h-[400px] mt-[-400px] ml-[1200px]">
         <img src="\programmer-image.jpg" className="small-logo" />
       </div>
 
@@ -108,6 +110,7 @@ export default function Home() {
       <div className="m-[200px] text-white font-bold text-4xl">
         <h3>Python-Projects</h3>
       </div>
+
     </>
   );
 }

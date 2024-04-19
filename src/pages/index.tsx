2e36bd3d8c3058d5466d9e26edf70163047214a4
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-19 22:15:00 +0200

    new sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,51 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect";
 import { Button } from "@/components/ui/button";
 import { StickyScroll } from "../components/ui/scroll-reveal";
 import { RefObject } from "react";
 import { BentoGrid, BentoGridItem } from "../components/ui/BentoGridItem";
-import {
-  IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
+import Gist from "react-gist";
+
+interface SidebarContent {
+  title: string;
+  gistId: string;
+}
 
 export default function Home() {
-  // Refs für die Verweise zu den Abschnitten der Seite
   const webDevelopmentRef = useRef<HTMLDivElement>(null);
   const pythonProjectsRef = useRef<HTMLDivElement>(null);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarContent, setSidebarContent] = useState<SidebarContent | null>(null);
 
-  // Scroll-Funktion für das Scrollen zu einem bestimmten Abschnitt der Seite
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (sidebarOpen && target.closest && !target.closest(".sidebar")) {
+        setSidebarOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [sidebarOpen]);
+
   const scrollToRef = (ref: RefObject<HTMLDivElement> | null) => {
     if (ref && ref.current) {
       window.scrollTo({
-        top: ref.current.offsetTop - window.innerHeight / 15,
+        top: ref.current.offsetTop - window.innerHeight / 10.9,
         behavior: "smooth",
       });
     } else {
-      window.scrollTo({ top: 0, behavior: "smooth" }); // Wenn ref null ist, scrollt zur Spitze der Seite
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }
+  };
+
+  const handleItemClick = (item: SidebarContent) => {
+    setSidebarContent(item);
+    setSidebarOpen(true);
   };
 
   const words = [
@@ -38,7 +55,6 @@ export default function Home() {
     },
   ];
 
-  // CONTACT ME BUTTON FUNCTION
   const handleContactMeClick = () => {
     window.location.href = "mailto:leoradtke@outlook.de";
   };
@@ -49,37 +65,48 @@ export default function Home() {
 
   const items = [
     {
-      title: "The Dawn of Innovation",
-      description: "Explore the birth of groundbreaking ideas and inventions.",
-      header: <Skeleton />,
-      icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Digital Revolution",
-      description: "Dive into the transformative power of technology.",
-      header: <Skeleton />,
-      icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Art of Design",
-      description: "Discover the beauty of thoughtful and functional design.",
-      header: <Skeleton />,
-      icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-      title: "The Power of Communication",
+      title: "Scrape API of NFT market",
       description:
-        "Understand the impact of effective communication in our lives.",
-      header: <Skeleton />,
-      icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+        "My Python program utilizes an API to conduct NFT analysis within the NFT market. Click on this field to show a code snippet ",
+      header: (
+        <img
+          src="\api.png"
+          className="w-[400px] h-[] mt-[40px]"
+          alt="Header Image"
+        />
+      ),
+      gistId: "2eb584a08d225bff5c9481ab5733ec7d",
     },
     {
-      title: "The Pursuit of Knowledge",
-      description: "Join the quest for understanding and enlightenment.",
-      header: <Skeleton />,
-      icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+      title: "Ceaser encryption ",
+      description:
+        "This code can proficiently encrypt a text file, enabling instant encryption upon request.",
+      header: (
+        <img
+          src="\ceaserimage.png"
+          className="w-[300px] h-[] mt-[-5px]"
+          alt="Header Image"
+        />
+      ),
+      gistId: "your-gist-id-for-ceaser-encryption",
     },
+
+    {
+      title: "text",
+      description:
+        "This code can proficiently encrypt a text file, enabling instant encryption upon request.",
+      header: (
+        <img
+          src="\ceaserimage.png"
+          className="w-[300px] h-[] mt-[-5px]"
+          alt="Header Image"
+        />
+      ),
+      gistId: "your-gist-id-for-ceaser-encryption",
+    },
+    // Weitere Elemente hier einfügen...
   ];
+  
 
   const content = [
     {
@@ -171,23 +198,75 @@ export default function Home() {
         <h3 className="underline">Python-Projects</h3>
       </div>
 
+      {/* Hier das BentoGrid einfügen */}
       <div
         className="p-10  30mt-[50px] ml-[450px]"
         style={{ maxWidth: "1000px" }}
       >
         <BentoGrid className="max-w-4xl mx-auto">
           {items.map((item, i) => (
-            <BentoGridItem
+            <div
               key={i}
-              title={item.title}
-              description={item.description}
-              header={item.header}
-              icon={item.icon}
-              className={i === 3 || i === 6 ? "md:col-span-2" : ""}
-            />
+              className={
+                i === 3 || i === 6
+                  ? "md:col-span-2 cursor-pointer"
+                  : "cursor-pointer"
+              }
+              onClick={(e) => {
+                e.stopPropagation(); // Klickereignis stoppen
+                handleItemClick(item); // Aktualisiert: handleItemClick aufrufen, um die Seitenleiste zu öffnen
+              }}
+            >
+              <BentoGridItem
+                title={item.title}
+                description={item.description}
+                header={item.header}
+                className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+              />
+            </div>
           ))}
         </BentoGrid>
       </div>
+
+      {/* Hier die Seitenleiste einfügen */}
+      {sidebarOpen && (
+        <div
+          className="bg-white fixed left-0 top-0 h-full w-1/4 overflow-y-auto"
+          style={{ width: "25%" }}
+        >
+          <div className="mt-[10px] ml-[400px]">
+            <button
+              className="flex items-center px-3 py-2 bg-white rounded-full hover:bg-gray-200"
+              onClick={() => setSidebarOpen(false)} // Schließe die Seitenleiste beim Klick auf den Button
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-12 h-12 text-gray-600" // Geändert auf w-8 und h-8 für größeres Symbol
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 9.293a1 1 0 011.414 0l4 4a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 111.414 1.414l-4 4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Dynamischer Seitenleisteninhalt */}
+          {sidebarContent && (
+            <>
+              <h3 className="font-bold mt-[-40px] ml-4 text-2xl">
+                {sidebarContent.title}
+              </h3>
+              <div className="gist mt-4">
+                <Gist id={sidebarContent.gistId} />
+              </div>
+            </>
+          )}
+        </div>
+      )}
       <nav className="fixed top-0 left-0 right-0 text-white flex items-center justify-center py-4 font-bold text-[19px] ">
         <ul className="flex space-x-11">
           <li

c4da45cff098283e8940f538b31bf4021348ddad
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-05-01 13:21:21 +0200

    mobile device + improvement + socials

---
+++ src/components/ui/experience.tsx
@@ -0,0 +1,29 @@
+import { cn } from "../../lib/utils";
+import React from "react";
+import { motion } from "framer-motion";
+
+export const BackgroundGradient = ({
+  children,
+  className,
+  containerClassName,
+  animate = true,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+  containerClassName?: string;
+  animate?: boolean;
+}) => {
+  const variants = {
+    initial: {
+      backgroundPosition: "0 50%",
+    },
+    animate: {
+      backgroundPosition: ["0, 50%", "100% 50%", "0 50%"],
+    },
+  };
+  return (
+    <div className={cn("relative p-[4px] group ml-[200px] mt-[100px] mb-[200px]  ", containerClassName)}>
+      <div className={cn("relative z-10", className)}>{children}</div>
+    </div>
+  );
+};
